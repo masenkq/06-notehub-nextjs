9d63ac09f3a7c721bd08633415ee2bcb56af0c0f
masenkq <stepamaria8@gmail.com>
2025-10-05 13:52:58 +0200

    fgh

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,28 +1,5 @@
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { fetchNotes } from '@/lib/api/api';
-import NotesClient from '../Notes.client'; // ← změněno z '../Notes.client' na './Notes.client'
-import { getQueryClient } from '@/lib/utils/getQueryClient';
+import NotesClient from './Notes.client';
 
 export default async function NotesPage() {
-  const queryClient = getQueryClient();
-  
-  try {
-    await queryClient.prefetchQuery({
-      queryKey: ['notes', '', 1],
-      queryFn: () => fetchNotes('', 1),
-    });
-
-    return (
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <NotesClient />
-      </HydrationBoundary>
-    );
-  } catch (error) {
-    console.error('Error prefetching notes:', error);
-    return (
-      <div>
-        <p>Error loading notes</p>
-      </div>
-    );
-  }
+  return <NotesClient />;
 }
